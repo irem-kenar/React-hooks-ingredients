6c9b834a36fcf1f3059802d6d81f8b2a357fab10
Irem Kenar <ikenar@kazandirio-dev.com>
2022-11-20 15:48:27 +0300

    fix useState function and added prevInputState v2

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -16,11 +16,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => useState[1]({ title: event.target.value, amount: inputState[0].amount })} />
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1](prevInputState => ({ title: event.target.value, amount: prevInputState.amount }))} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => useState[1]({ amount: event.target.value, title: inputState[0].title })} />
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1](prevInputState => ({ amount: event.target.value, title: prevInputState.title }))} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
