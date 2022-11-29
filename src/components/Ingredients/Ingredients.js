4efd67a64126f2cfc5a71e16c26df08cfa9924a8
Irem Kenar <ikenar@kazandirio-dev.com>
2022-11-29 20:48:07 +0300

    added useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -8,6 +8,27 @@ import Search from './Search';
 function Ingredients() {
 
   const [userIngredients, setUserIngredients] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json', {
