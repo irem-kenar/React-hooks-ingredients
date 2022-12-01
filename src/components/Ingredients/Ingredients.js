22b6be84ccb72673cba4fbd99c9e08ce0241e8c0
Irem Kenar <ikenar@kazandirio-dev.com>
2022-12-01 19:55:25 +0300

    got rid of extra render cycle

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -11,22 +11,6 @@ function Ingredients() {
 
 
   useEffect(() => {
-    fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
-
-  useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
