41d09dd49e0f8f720f4f7407943bd530839c8f80
Irem Kenar <ikenar@kazandirio-dev.com>
2022-12-01 19:54:06 +0300

    added useCallback to avoid infinite loop

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -30,9 +30,9 @@ function Ingredients() {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
-  const filteredIngredientsHandler = filteredIngredients => {
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
     setUserIngredients(filteredIngredients);
-  }
+  }, [])
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json', {
