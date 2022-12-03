d2e32b539f4f7fcb5ac54465ddab493b2a34799f
Irem Kenar <ikenar@kazandirio-dev.com>
2022-12-03 17:56:34 +0300

    added custom hook for http request

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,6 +4,7 @@ import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
+import useHttp from '../../hooks/http';
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
@@ -17,36 +18,55 @@ const ingredientReducer = (currentIngredients, action) => {
       throw new Error('Should not get there!');
   }
 };
-
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { loading: true, error: null };
-    case 'RESPONSE':
-      return { ...curHttpState, loading: false };
-    case 'ERROR':
-      return { loading: false, error: action.errorMessage };
-    case 'CLEAR':
-      return { ...curHttpState, error: null };
-    default:
-      throw new Error('Should not be reached!');
-  }
-};
+// this is a hook now
+// const httpReducer = (curHttpState, action) => {
+//   switch (action.type) {
+//     case 'SEND':
+//       return { loading: true, error: null };
+//     case 'RESPONSE':
+//       return { ...curHttpState, loading: false };
+//     case 'ERROR':
+//       return { loading: false, error: action.errorMessage };
+//     case 'CLEAR':
+//       return { ...curHttpState, error: null };
+//     default:
+//       throw new Error('Should not be reached!');
+//   }
+// };
 
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  const [httpState, dispatchHttp] = useReducer(httpReducer, {
-    loading: false,
-    error: null
-  });
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    reqIdentifer
+  } = useHttp();
+
+  // this is a hook now
+  // const [httpState, dispatchHttp] = useReducer(httpReducer, {
+  //   loading: false,
+  //   error: null
+  // });
+
   // const [userIngredients, setUserIngredients] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
   // const [error, setError] = useState();
 
 
   useEffect(() => {
-    console.log('RENDERING INGREDIENTS', userIngredients);
-  }, [userIngredients]);
+    if (!isLoading && !error && reqIdentifer === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra });
+    } else if (!isLoading && !error && reqIdentifer === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: data.name, ...reqExtra }
+      });
+    }
+  }, [data, reqExtra, reqIdentifer, isLoading, error]);
+
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
     // setUserIngredients(filteredIngredients);
@@ -54,51 +74,67 @@ const Ingredients = () => {
   }, [])
 
   const addIngredientHandler = useCallback(ingredient => {
+    sendRequest(
+      'https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    );
     //setIsLoading(true);
-    dispatchHttp({ type: 'SEND' });
-    fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(response => {
-      //setIsLoading(false);
-      dispatchHttp({ type: 'RESPONSE' });
-      return response.json();
-    }).then(responseData => {
-      // setUserIngredients(prevIngredients => [
-      //   ...prevIngredients,
-      //   { id: responseData.name, ...ingredient }
-      // ]);
-      dispatch({
-        type: 'ADD',
-        ingredient: { id: responseData.name, ...ingredient }
-      });
-    })
+    // this is a hook now
+    //dispatchHttp({ type: 'SEND' });
+    // fetch('https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients.json', {
+    //   method: 'POST',
+    //   body: JSON.stringify(ingredient),
+    //   headers: { 'Content-Type': 'application/json' }
+    // }).then(response => {
+    //   //setIsLoading(false);
+    //   dispatchHttp({ type: 'RESPONSE' });
+    //   return response.json();
+    // }).then(responseData => {
+    //   // setUserIngredients(prevIngredients => [
+    //   //   ...prevIngredients,
+    //   //   { id: responseData.name, ...ingredient }
+    //   // ]);
+    //   dispatch({
+    //     type: 'ADD',
+    //     ingredient: { id: responseData.name, ...ingredient }
+    //   });
+    // })
   }, []);
 
   const removeIngredientHandler = useCallback(ingredientId => {
     //setIsLoading(true);
-    dispatchHttp({ type: 'SEND' });
-    fetch(`https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE',
-    }).then(response => {
-      //setIsLoading(false);
-      dispatchHttp({ type: 'RESPONSE' });
-      // setUserIngredients(prevIngredients =>
-      //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-      // )
-      dispatch({ type: 'DELETE', id: ingredientId });
-    }).catch(error => {
-      dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' });
-      // setError(error.message)
-      // setError('Something went wrong');
-      // setIsLoading(false)
+    sendRequest(
+      `https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      'DELETE',
+      null,
+      ingredientId,
+      'REMOVE_INGREDIENT'
+    );
+  },
+    // dispatchHttp({ type: 'SEND' });
+    // fetch(`https://react-hooks-update-26a09-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
+    //   method: 'DELETE',
+    // }).then(response => {
+    //   //setIsLoading(false);
+    //   dispatchHttp({ type: 'RESPONSE' });
+    //   // setUserIngredients(prevIngredients =>
+    //   //   prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    //   // )
+    //   dispatch({ type: 'DELETE', id: ingredientId });
+    // }).catch(error => {
+    //   dispatchHttp({ type: 'ERROR', errorMessage: 'Something went wrong!' });
+    //   // setError(error.message)
+    //   // setError('Something went wrong');
+    //   // setIsLoading(false)
 
-    });
-  }, []);
+    // });
+    [sendRequest]);
 
   const clearError = useCallback(() => {
-    dispatchHttp({ type: 'CLEAR' });
+    //dispatchHttp({ type: 'CLEAR' });
     //setError(null);
   }, [])
 
@@ -108,8 +144,8 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading} />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
         {ingredientList}
